4b9ff1561bff1c7e034cedea8b502e17cf449822
Justus Soh <justus.soh@hotmail.com>
2019-06-29 15:59:08 +0800

    PollView Fixed for Mcq Type

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -10,7 +10,7 @@ import { Chart } from 'react-google-charts';
 
 import { Comment } from '../components/PollingForm'
 
-const keyTypes= ['title', 'imgSrc', 'pollType']
+const keyTypes = ['title', 'imgSrc', 'pollType']
 
 class Poll extends React.Component {
     constructor(props) {
@@ -20,7 +20,7 @@ class Poll extends React.Component {
         this.state = {
             title: '',
             options: [],
-            imgSrc:null,
+            imgSrc: null,
             voted: localStorage.getItem(this.props.match.params.pollId) ? true : false,
             showSnackbar: false,
             loading: true
@@ -32,15 +32,20 @@ class Poll extends React.Component {
         console.log(this.pollRef)
         this.pollRef.on('value', ((snapshot) => {
             const dbPoll = snapshot.val();
+            if (dbPoll.pollType === 'mcq') {
+                const options = Object.keys(dbPoll).reduce((a, key) => {
+                    if (!keyTypes.includes(key)) {
+                        a.push({ [key]: dbPoll[key] });
+                    }
+                    return a;
+                }, []);
 
-            const options = Object.keys(dbPoll).reduce((a, key) => {
-                if (!keyTypes.includes(key)) {
-                    a.push({ [key]: dbPoll[key] });
-                }
-                return a;
-            }, []);
+                this.setState({ title: dbPoll.title, options: options, imgSrc: dbPoll.imgSrc, pollType: dbPoll.pollType, loading: false })
+            }
 
-            this.setState({ title: dbPoll.title, options: options, imgSrc:dbPoll.imgSrc, pollType: dbPoll.pollType, loading: false })
+            if(dbPoll.pollType === 'open'){
+                this.setState({ title: dbPoll.title, imgSrc: dbPoll.imgSrc, pollType: dbPoll.pollType, loading: false })
+            }
         })).bind(this);
     }
 
@@ -59,15 +64,13 @@ class Poll extends React.Component {
     }
 
     render() {
-        //[["Task","Hours per Day"],["Work",11],["Eat",2],["Commute",2],["Watch TV",2],["Sleep",7]]
+
         const data = this.state.options.map(option => {
             return [Object.keys(option)[0], option[Object.keys(option)[0]]];
         });
         data.unshift(['option', 'votes']);
 
-        //let isAuthUser = getLocalUserId() ? true : false;
         let isAuthUser = firebaseApp.auth().currentUser ? true : false;
-        //var isAuthUser = true;
 
         let addOptionUI;
         if (isAuthUser) {
@@ -90,56 +93,62 @@ class Poll extends React.Component {
                         onClick={() => this.handleVote(Object.keys(option)[0])}
                         disabled={this.state.voted}
                         variant='contained'
-                        />
+                    />
                     <br /><br />
                 </div>
             );
         });
 
-        return (
-            <div className="row">
-                <div className="col-sm-12 text-xs-center">
-
-                    <Snackbar
-                        open={this.state.showSnackbar}
-                        message="Thanks for your vote!"
-                        autoHideDuration={4000}
-                        />
-
-                    <Paper>
-                        <br /><br />
-                        <h2>{this.state.title}</h2>
-                        <br />
-
-                        {this.state.imgSrc !== null ?
-                                <div>
-                                    <img src={this.state.imgSrc} alt='User Uploaded' />
-                                </div> : '' }
-
-                        <Loading loading={this.state.loading} />
-
-                        {optionsUI}
-
-                        {addOptionUI}
-
-                        <br />
-                        <Chart
-                            chartTitle="DonutChart"
-                            chartType="PieChart"
-                            width="100%"
-                            data={data}
-                            options={{ is3D: 'true' }}
-                            />
-
-                        <br /><br />
-
-                        <Comment pollId={this.props.match.params.pollId}/>
-
-                    </Paper>
-                </div>
-            </div>
-        );
+        switch (this.state.pollType){
+            case 'mcq':
+                    return (
+                        <div className="row">
+                            <div className="col-sm-12 text-xs-center">
+            
+                                <Snackbar
+                                    open={this.state.showSnackbar}
+                                    message="Thanks for your vote!"
+                                    autoHideDuration={4000}
+                                />
+            
+                                <Paper>
+                                    <br /><br />
+                                    <h2>{this.state.title}</h2>
+                                    <br />
+            
+                                    {this.state.imgSrc !== null ?
+                                        <div>
+                                            <img src={this.state.imgSrc} alt='User Uploaded' />
+                                        </div> : ''}
+            
+                                    <Loading loading={this.state.loading} />
+            
+                                    {optionsUI}
+            
+                                    {addOptionUI}
+            
+                                    <br />
+                                    <Chart
+                                        chartTitle="DonutChart"
+                                        chartType="PieChart"
+                                        width="100%"
+                                        data={data}
+                                        options={{ is3D: 'true' }}
+                                    />
+            
+                                    <br /><br />
+            
+                                    <Comment pollId={this.props.match.params.pollId} />
+            
+                                </Paper>
+                            </div>
+                        </div>
+                        );
+            default:
+                return (<h1>Unknown Poll Type</h1>);
+        }
+             
     }
 }
 
-export {Poll};
+export { Poll };
