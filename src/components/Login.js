4efaac5b1ef2cdc9c855e94bcd1e05fce9ba0f4f
Justus Soh <justus.soh@hotmail.com>
2019-06-30 21:27:12 +0800

    Demostration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { firebaseApp } from '../utils/firebase';
-import * as firebase from 'firebase'; 
+import * as firebase from 'firebase';
 import history from '../history';
 
 import Icon from '@mdi/react';
-import {  mdiFacebookBox, mdiGoogle } from '@mdi/js';
+import { mdiFacebookBox, mdiGoogle } from '@mdi/js';
 
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid'
+
+import styled from 'styled-components';
+
+const Styles = styled.div`
+  .center{
+    color:blue;
+    text-align:center;
+  }
+
+    
+`;
 
 class Login extends React.Component {
 
@@ -33,50 +45,54 @@ class Login extends React.Component {
     });
   }
 
-  
+
 
   render() {
     return (
-      <div className="row">
-        <div className="col-sm-12 text-xs-center">
+      <Styles>
+        <div>
+          <Grid container justify="center" alignItems="center">
+            <Grid item xs={4}>
 
-          <Paper>
-          
+              <Paper>
 
-            <br /><br />
-            <h2>Create and share polls, fast and easy. View results in real time!</h2>
 
-            <br /><br />
-            <Button
-              onClick={this.handleFacebook}>
-              <Icon path={mdiFacebookBox} size={1}/> 
-              Login with Facebook
+                <br /><br />
+                <h2 className='center'>Create and share polls, fast and easy. View results in real time!</h2>
+
+                <br /><br />
+                <Button
+                  onClick={this.handleFacebook}>
+                  <Icon path={mdiFacebookBox} size={1}/>
+                  Login with Facebook
             </Button>
-            
-            <br /><br />
-            <Button
-              onClick={this.handleGoogle}>
-                   <Icon path = {mdiGoogle} size={1}/>
-                   Login with Google
-            </Button>
-              
-            <br /><br />
-              <Button
-                href="/loginpage"
-                children="Login with Email"
-                />
-            <br /><br />
-              <Button
-                href="/signup"
-                children="Sign Up"
-                />
-            <br /><br />
-            <br /><br />
-          
-          </Paper>
 
+                <br /><br />
+                <Button
+                  onClick={this.handleGoogle}>
+                  <Icon path={mdiGoogle} size={1} />
+                  Login with Google
+            </Button>
+
+                <br /><br />
+                <Button
+                  href="/loginpage"
+                  children="Login with Email"
+                />
+                <br /><br />
+                <Button
+                  href="/signup"
+                  children="Sign Up"
+                />
+                <br /><br />
+                <br /><br />
+
+              </Paper>
+            </Grid>
+          </Grid>
         </div>
-      </div>
+      </Styles>
+
 
     );
   }
